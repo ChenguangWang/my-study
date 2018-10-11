146f06711a426aa51ed3c7e3f48c7e9d001d4ceb
Wang Chenguang <chenguang.wang@tendcloud.com>
2018-10-11 17:26:09 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { CKEditorModule } from 'ng2-ckeditor';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AdBannerComponent } from './ad-banner.component';
+import { HeroJobAdComponent } from './hero-job-ad.component';
+import { HeroProfileComponent } from './hero-profile.component';
+import { AdDirective } from './ad.directive';
+import { AdService } from './ad.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AdBannerComponent,
+        HeroJobAdComponent,
+        HeroProfileComponent,
+        AdDirective,
+    ],
+    imports: [
+        BrowserModule,
+        CKEditorModule,
+        FormsModule
+    ],
+    entryComponents: [HeroJobAdComponent, HeroProfileComponent],
+    providers: [AdService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
