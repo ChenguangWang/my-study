607b124898ec4df0c88aff1851d1bb9412854e64
Wang Chenguang <chenguang.wang@tendcloud.com>
2020-05-07 15:58:28 +0800

    commit

--- src/app/ck-editor/editor.component.ts
+++ src/app/ck-editor/editor.component.ts
@@ -1,74 +1,67 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 declare var CKEDITOR: any;
 
 @Component({
-    selector: 'app-editor',
-    templateUrl: './editor.component.html',
+  selector: "app-editor",
+  templateUrl: "./editor.component.html"
 })
 export class EditorComponent implements OnInit {
+  config: any = {
+    width: "400px",
+    // height: '158px',
+    height: "358px",
+    toolbar: [
+      // 工具栏
+      ["Font", "FontSize", "TextColor", "-", "Bold", "Italic", "Underline"]
+    ],
+    removePlugins: "elementspath",
+    resize_enabled: false,
+    disableAutoInline: true
+  };
 
-    config: any = {
-        width: '400px',
-        // height: '158px',
-        height: '358px',
-        toolbar: [ // 工具栏
-            ['Font', 'FontSize', 'TextColor', '-', 'Bold', 'Italic', 'Underline']
-        ],
-        removePlugins: 'elementspath',
-        resize_enabled: false,
-        disableAutoInline: true
-    };
+  content: any;
 
-    content: any;
+  @ViewChild("editor") editor: any;
 
+  constructor() {}
 
-    @ViewChild('editor') editor: any;
+  ngOnInit() {
+    const that = this;
+    document.getElementById("div").addEventListener("scroll", function(e) {
+      let a = that.editor.instance;
+      // 聚焦
+      a.focus();
 
-    constructor() { 
+      // 将光标移至最末
+      var range = a.createRange();
+      range.moveToElementEditEnd(a.editable());
 
-    }
+      range.select();
+      range.scrollIntoView();
+    });
+  }
 
-    ngOnInit() {
-        const that = this;
-        document.getElementById('div').addEventListener("scroll", function (e) {
-            let a = that.editor.instance;
-            // 聚焦
-            a.focus();
+  ckeditorContent = '<p><span style="color:#c0392b">啦啦啦啦啦</span></p>';
 
-            // 将光标移至最末
-            var range = a.createRange();
-            range.moveToElementEditEnd(a.editable());
-
-            range.select();
-            range.scrollIntoView();
-        })
-    }
-
-    ckeditorContent = '<p><span style="color:#c0392b">啦啦啦啦啦</span></p>';
-
-    test() {
-        // Get the editor instance that we want to interact with.
-        var oEditor = CKEDITOR.instances.editor1;
-        var value = `<a href='#"'>Solo</a>`;
-        // Check the active editing mode.
-        if (oEditor.mode == 'wysiwyg') {
-            // Insert HTML code.
-            oEditor.insertHtml(value);
-        }
-        else
-            alert('You must be in WYSIWYG mode!');
-    }
-    InsertText() {
-        // Get the editor instance that we want to interact with.
-        var oEditor = CKEDITOR.instances.editor1;
-        var value = "Solo";
-        // Check the active editing mode.
-        if (oEditor.mode == 'wysiwyg') {
-            // Insert as plain text.
-            oEditor.insertText(value);
-        }
-        else
-            alert('You must be in WYSIWYG mode!');
-    }
+  test() {
+    // Get the editor instance that we want to interact with.
+    let oEditor = CKEDITOR.instances.editor1;
+    let value = `<a href='#"'>Solo</a>`;
+    // Check the active editing mode.
+    if (oEditor.mode == "wysiwyg") {
+      // Insert HTML code.
+      oEditor.insertHtml(value);
+    } else alert("You must be in WYSIWYG mode!");
+  }
+  InsertText() {
+    // Get the editor instance that we want to interact with.
+    let oEditor = CKEDITOR.instances.editor1;
+    let value = "Solo";
+    // Check the active editing mode.
+    if (oEditor.mode == "wysiwyg") {
+      // Insert as plain text.
+      oEditor.insertText(value);
+    } else alert("You must be in WYSIWYG mode!");
+  }
 }
